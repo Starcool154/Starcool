866ce7ae9b141df4b44c266e6bf3651e347ac9dc
Leonardo Melgarejo Aguila <leopoldonaldo223@gmail.com>
2019-08-06 20:38:42 +0200

    Creacion de componentes

--- myfirstapp/src/app/app.component.ts
+++ myfirstapp/src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'El poldo';
+  name = 'Leo';
+  surname = 'Melgarejo Aguila';
 }
--- myfirstapp/src/app/app.module.ts
+++ myfirstapp/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BodyComponent } from './body/body.component';
+import { CardComponent } from './card/card.component';
+import {MatCardModule} from '@angular/material/card'; 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    CardComponent
   ],
-  imports: [
+  imports: [FormsModule, MatToolbarModule, MatCardModule,
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
